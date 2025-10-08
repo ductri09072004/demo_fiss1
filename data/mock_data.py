d4d9204d69fe5f59fc7a9e114177d19995d5cf88
BlueDuck <145476276+ductri09072004@users.noreply.github.com>
2025-10-08 10:28:24 +0700

    feat: v12

--- data/mock_data.py
+++ data/mock_data.py
@@ -9,7 +9,7 @@ def get_system_info():
     """Trả về thông tin hệ thống giả"""
     return {
         "system_info": {
-            "name": "Demo FISS API v11",
+            "name": "Demo FISS API v12",
             "version": "1.0.7",
             "environment": "development"
         },
